05182af88cf3779058e5deccf6a1278b79ff1314
Mark Tai <mark@marktai.com>
2024-04-16 01:17:41 -0700

    clean out stale comments

--- backend/src/games/admin.py
+++ backend/src/games/admin.py
@@ -4,16 +4,6 @@ from . import models
 
 # Register your models here.
 
-# @admin.register(models.Game)
-# class GameAdmin(admin.ModelAdmin):
-#     fields = ('white_player', 'black_player', 'board_link', 'turn_count', 'created_time', 'last_updated_time')
-#     readonly_fields = ('board', 'board_link', 'turn_count', 'created_time', 'last_updated_time')
-
-#     def board_link(self, obj):
-#         url = '../../../board/%d/change' % obj.board.id
-#         return mark_safe('<a href="%s">%s</a>' % (url, obj.board.fen))
-
-
 @admin.register(models.Expense)
 class ExpenseAdmin(admin.ModelAdmin):
     pass
